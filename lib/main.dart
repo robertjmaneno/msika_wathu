22c6fdc6cf364264e39942ae0fc0750b3eef6067
Robert J Maneno <robertjmaneno@gmail.com>
2023-08-22 14:25:19 +0200

    adding the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'msika_wathu',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
